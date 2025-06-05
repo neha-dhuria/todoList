eb214a5cf9742bfdc2270d34cbd431271cc316ef
neha-dhuria <neha.dhuria92@gmail.com>
2025-06-05 13:03:54 +0530

    Second Comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { tabs } from './Data/tabs';
 
 function App() {
+  //tabs ----->
+
+  let [activeTabs,setActiveTabs]=useState(0);
+  let [activeContent,setActiveContent]=useState(tabs[0])
+
+  let changeData=(index)=>{
+    setActiveTabs(index);
+    setActiveContent(tabs[index]);
+  }
+
+
+  //todo------>
+  let [todolist,setTodolist]=useState([]);
+
+
+  let saveToDoList=(event)=>{
+
+    let toname=event.target.toname.value;
+
+    if(!todolist.includes(toname))
+    {
+      let finalDolist=[...todolist,toname];
+      setTodolist(finalDolist);
+    }
+    else{
+      alert("Todo already exists");
+    }
+    
+    event.preventDefault();
+  }
+
+  let list=todolist.map((value,index)=>{
+    return(
+      <ToDoListItems value={value} key={index} indexNumber={index} 
+      todolist={todolist}
+      setTodolist={setTodolist}/>
+          
+    )
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* tabbing-------> */}
+      <div className='tabsOuter'>
+          <h1 style={{textAlign:'left'}}>
+            Law Prep Vission , Mission and Values
+          </h1>
+          <ul>
+            {tabs.map((tabsItems,index)=>{
+              return(
+                <li>
+                  <button onClick={()=>changeData(index)} className={activeTabs==index ? 'activeButton' : ''}>{tabsItems.title}</button>
+                </li>
+              )
+            })}
+          </ul>
+          {activeContent!==undefined?
+            <p>
+              {activeContent.description}
+            </p>
+          :''}
+      </div>
+    {/* todolist-----> */}
+      <h1>    
+        ToDo List
+      </h1>
+      <form onSubmit={saveToDoList}>
+        <input type='text' name='toname'/> 
+        <button>Save</button>
+      </form>
+      <div className='outerDiv'>
+        <ul>
+          {list}
+          
+        </ul>
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({value , indexNumber,todolist,setTodolist}){
+  //indexNumber---> where you work
+  //todolist ---->full list
+  //setToDolist ---->function which updates the full list
+
+  let[status,setStatus] = useState(false);
+  let deleteRow=()=>{
+    let finalData=todolist.filter((v,i)=>i!=indexNumber);
+    //console.log(finalData);
+    setTodolist(finalData);
+  }
+
+  let checkStatus=()=>{
+    setStatus(!status);
+  }
+
+  return(
+      <li className={(status)? 'completetodo' : ''} onClick={checkStatus}>{indexNumber+1} {value} <span onClick={deleteRow}>&times;</span></li>
+  )
+}
---
+++ src/Data/tabs.jsx
@@ -0,0 +1,15 @@
+export let tabs=[
+    {
+        'title' : 'Vision',
+        'description': 'Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.'
+    },
+    {
+        'title' : 'Mission',
+        'description': 'Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.'
+    },
+    {
+        'title' : 'Values',
+        'description': 'Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.Vision is the complex, multipart process that gives you your sense of sight. Vision itself does not happen in your eyes. Instead, it is the sum of your eyes, retinas, optic nerves and brain working together to process reflected light from the world around you.'
+    }
+    
+]
